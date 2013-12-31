6f97f2fdb95067da59f0beeb15dd37f0012e9339
oneplus1000 <oneplus1000@gmail.com>
2013-12-31 11:05:47 +0700

    underline (init)

---
+++ underlinepdfhandler.go
@@ -0,0 +1,24 @@
+package gopdfusecase
+
+import (
+	"net/http"
+	"github.com/signintech/gopdf"
+	"github.com/signintech/gopdf/fonts"
+)
+
+type UnderlinePdfHandler struct{
+
+}
+
+
+func (me *UnderlinePdfHandler) ServeHTTP(w http.ResponseWriter,r *http.Request){
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{Unit: "pt", PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 = A4
+	pdf.AddFont("THSarabunPSK",new(fonts.THSarabun),Path_ResFont("THSarabun.z"))
+	pdf.AddFont("Loma",new(fonts.Loma),Path_ResFont("Loma.z"))
+	pdf.AddPage()
+	pdf.SetFont("THSarabunPSK","U",14)
+	pdf.Cell(nil,   Encoder_Utf8ToCp874("Hello world  = สวัสดี โลก in thai"))
+	w.Header().Set("Content-type", "application/pdf")
+	w.Write(pdf.GetBytesPdf())
+}
