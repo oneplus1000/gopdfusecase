7f6d4bdb2cd8bedde64a31e8ae2a84f515759479
oneplus1000 <oneplus1000@gmail.com>
2015-04-15 17:07:01 +0700

    ...

--- noembedpdfhandler.go
+++ noembedpdfhandler.go
@@ -1,6 +1,7 @@
 package gopdfusecase
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/signintech/gopdf"
@@ -13,8 +14,13 @@ func (me *NoembedPdfHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	pdf := gopdf.GoPdf{}
 	pdf.Start(gopdf.Config{Unit: "pt", PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 = A4
 	pdf.AddPage()
+	err := pdf.AddFontUnicode("Loma", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/Loma.ttf")
+	//err := pdf.AddFontUnicode("Refine", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/RefinementSuperSongStyleGBKFontSimplifiedChinese.TTF")
+	if err != nil {
+		log.Printf("ERROR:%s\n", err.Error())
+	}
 	pdf.SetFont("Loma", "B", 14)
-	pdf.Cell(nil, Encoder_Utf8ToCp874("Hello world  = สวัสดี โลก in thai"))
+	pdf.Cell(nil, Encoder_Utf8ToCp874("ก"))
 	w.Header().Set("Content-type", "application/pdf")
 	b := pdf.GetBytesPdf()
 	w.Write(b)
