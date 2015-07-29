c61c2c5c99bc58d2a5805beac9c3432f7b491049
oneplus1000 <oneplus1000@gmail.com>
2015-07-30 00:42:12 +0700

    add japan

--- noembedpdfhandler.go
+++ noembedpdfhandler.go
@@ -1,7 +1,6 @@
 package gopdfusecase
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -12,22 +11,63 @@ type NoembedPdfHandler struct {
 }
 
 func (me *NoembedPdfHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	pdf := gopdf.GoPdf{}
 	pdf.Start(gopdf.Config{Unit: "pt", PageSize: gopdf.Rect{W: 595.28, H: 841.89}}) //595.28, 841.89 = A4
 	pdf.AddPage()
-	err := pdf.AddTTFFont("Loma", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/Loma.ttf")
+
+	var err error
+	//err = pdf.AddTTFFont("HDZB_5", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/aa.ttf")
+	err = pdf.AddTTFFont("HDZB_5", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/wts11.ttf")
 	if err != nil {
 		log.Printf("ERROR:%s\n", err.Error())
+		return
 	}
+
+	err = pdf.AddTTFFont("Loma", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/Loma.ttf")
+	if err != nil {
+		log.Printf("ERROR:%s\n", err.Error())
+		return
+	}
+
+	//BokutachinoGothic2Regular.ttf
+	err = pdf.AddTTFFont("BokutachinoGothic2Regular", "/home/oneplus/GOPATH/src/github.com/signintech/gopdf/fontmaker/ttf/BokutachinoGothic2Regular.ttf")
+	if err != nil {
+		log.Printf("ERROR:%s\n", err.Error())
+		return
+	}
+
+	//Thai
 	err = pdf.SetFont("Loma", "B", 14)
 	if err != nil {
 		log.Printf("ERROR:%s\n", err.Error())
+		return
 	}
-	//log.Printf("ERROR")
-	pdf.Cell(nil, "กข")
-	pdf.Cell(nil, "คง")
+	pdf.Cell(nil, "Hello")
+	pdf.Br(20)
+	pdf.Cell(nil, "สวัสดี")
+	pdf.Br(20)
+
+	//China
+	err = pdf.SetFont("HDZB_5", "B", 14)
+	if err != nil {
+		log.Printf("ERROR:%s\n", err.Error())
+		return
+	}
+	pdf.Cell(nil, "您好")
+	pdf.Br(20)
+
+	//Japan
+	err = pdf.SetFont("BokutachinoGothic2Regular", "B", 14)
+	if err != nil {
+		log.Printf("ERROR:%s\n", err.Error())
+		return
+	}
+	pdf.Cell(nil, "こんにちは")
+	pdf.Br(20)
+
 	w.Header().Set("Content-type", "application/pdf")
 	b := pdf.GetBytesPdf()
-	fmt.Printf("byte=%s\n\n", string(b))
+	//fmt.Printf("byte=%s\n\n", string(b))
 	w.Write(b)
 }
--- path.go
+++ path.go
@@ -1,10 +1,9 @@
 package gopdfusecase
 
-func Path_Www(filename string) string{
-	return  "www/" + filename
+func Path_Www(filename string) string {
+	return "../www/" + filename
 }
 
 func Path_ResFont(fontnamefilename string) string {
 	return "res/fonts/" + fontnamefilename
 }
-
